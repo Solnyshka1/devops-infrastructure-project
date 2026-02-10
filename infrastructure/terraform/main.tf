82cf3295ef594de2d24972bdb841eef30032d4f8
Saadat Nurbekova <saadat.nurbekova1@alatoo.edu.kg>
2026-02-11 00:01:09 +0600

    Enhance Terraform configuration with security group

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -10,3 +10,14 @@ resource "aws_instance" "web" {
     Name = "DevOps-Web-Server"
   }
 }
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
